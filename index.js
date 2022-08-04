61d275f846243f644ebb2ce262a17d1435609f4d
Rob17591* <rluisnpt1@outlook.com>
2022-08-04 07:42:11 +0000

    Update 4 files

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const logger = require("./helpers/logger");
 const app = express();
 const PORT = 8000;
 
-var BBWEBHOOK_RECEIVE_ENDPOINT = "/webhook";
+var BBWEBHOOK_RECEIVE_ENDPOINT = "/webhooks";
 app.use(bodyParser.json());
 
 app.get(["/"], function (req, res) {
@@ -17,7 +17,7 @@ app.get(["/"], function (req, res) {
 
   console.log("Receive get webhook request: Full URL: %s - %s", url, csse);
   res.send({
-    message: "Received GET request. Check the console for more info"
+    message: "Received GET request. Check the console for more info",
   });
 });
 
@@ -25,12 +25,22 @@ app.get([BBWEBHOOK_RECEIVE_ENDPOINT], function (req, res) {
   const { url } = req;
   console.log("Receive get webhook request: Full URL: %s", url);
   res.send({
-    message: "Received GET request. Check the console for more info"
+    message: "Received GET request. Check the console for more info",
   });
 });
 
 app.post(BBWEBHOOK_RECEIVE_ENDPOINT, (req, res) => {
   const { body, url, method, headers } = req;
+
+  //The event key of the event that triggers the webhook (for example, repo:push or pullrequest:approved).
+  const eventKey = headers.get("x-event-key");
+  //The UUID of the webhook that an event triggers. For details about the UUID of a webhook, see the webhook Resource.
+  const hookUUID = headers.get("x-hook-uuid");
+  //X-Request-UUID The UUID of the request.
+  const requestUUID = headers.get("x-request-uuid");
+  // The number of times Bitbucket attempts to send the payload. After the first request, Bitbucket tries
+  // to send the payload two more times if the previous attempts fail.
+  const requestUUID = headers.get("x-attempt-number");
 
   console.log("POST Received webhook request to /webhook-receive");
   console.log("Request Headers: %s", headers);
@@ -40,13 +50,13 @@ app.post(BBWEBHOOK_RECEIVE_ENDPOINT, (req, res) => {
   logger.log({ level: "info", message: json });
 
   res.send({
-    message: "Received POST request. Check the console for more info"
+    message: "Received POST request. Check the console for more info",
   });
 });
 
 app.listen(PORT, function (err) {
   if (err) {
-    console.log(err);
+    logger.log(err);
   } else {
     console.log(
       `Gravity webhook example server listening at http://localhost:${PORT}`
